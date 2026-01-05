1397f13ba80d49c27972df57ebb4f3b402dba050
Belmouk <el.mehdi.boukhiam@gmail.com>
2026-01-05 12:15:42 +0100

    feature: build form and input components

---
+++ src/App.jsx
@@ -0,0 +1,190 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Form
+        title="General information"
+        inputs={[
+          {
+            tag: "input",
+            label: "Name",
+            type: "text",
+            name: "name",
+            id: "name",
+          },
+          {
+            tag: "input",
+            label: "Email",
+            type: "email",
+            name: "email",
+            id: "email",
+          },
+          {
+            tag: "input",
+            label: "Phone",
+            type: "tel",
+            name: "phone",
+            id: "phone",
+          },
+        ]}
+      ></Form>
+      <Form
+        title="Education"
+        inputs={[
+          {
+            tag: "input",
+            label: "School name",
+            type: "text",
+            name: "school",
+            id: "school",
+          },
+          {
+            tag: "input",
+            label: "Major",
+            type: "text",
+            name: "major",
+            id: "major",
+          },
+          {
+            tag: "input",
+            label: "Start date",
+            type: "date",
+            name: "school-start",
+            id: "school-start",
+          },
+          {
+            tag: "input",
+            label: "End date",
+            type: "date",
+            name: "school-end",
+            id: "school-end",
+          },
+        ]}
+      ></Form>
+      <Form
+        title="Professional Experience"
+        inputs={[
+          {
+            tag: "input",
+            label: "Company name",
+            type: "text",
+            name: "company-name",
+            id: "company-name",
+          },
+          {
+            tag: "input",
+            label: "Role",
+            type: "text",
+            name: "role",
+            id: "role",
+          },
+          {
+            tag: "textarea",
+            label: "Responsibilities",
+            type: undefined,
+            name: "responsibilities",
+            id: "responsibilities",
+          },
+          {
+            tag: "input",
+            label: "Start date",
+            type: "date",
+            name: "job-start-date",
+            id: "job-start-date",
+          },
+          {
+            tag: "input",
+            label: "End date",
+            type: "date",
+            name: "job-end-date",
+            id: "job-end-date",
+          },
+        ]}
+      ></Form>
+    </div>
+  );
+}
+
+function Form({ title, inputs }) {
+  const [isDisabled, setIsDisabled] = useState(false);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsDisabled(true);
+  }
+  function handleEdit(e) {
+    setIsDisabled(false);
+  }
+
+  return (
+    <form action="" autoComplete="on" onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>{title}</legend>
+        {inputs.map((item, index) => {
+          if (item.tag === "textarea") {
+            return (
+              <TextArea
+                {...item}
+                key={index}
+                isDisabled={isDisabled}
+              ></TextArea>
+            );
+          }
+          return <Input {...item} key={index} isDisabled={isDisabled}></Input>;
+        })}
+        <div>
+          <button type="submit">Submit</button>
+          <button type="button" onClick={handleEdit}>
+            Edit
+          </button>
+        </div>
+      </fieldset>
+    </form>
+  );
+}
+
+function Input({ label, type, name, id, isDisabled }) {
+  const [value, setValue] = useState("");
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return (
+    <label htmlFor={id}>
+      {label + " "}
+      <input
+        type={type}
+        name={name}
+        id={id}
+        value={value}
+        onChange={handleChange}
+        disabled={isDisabled}
+      />
+    </label>
+  );
+}
+
+function TextArea({ id, name, label, isDisabled }) {
+  const [value, setValue] = useState("");
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+  return (
+    <label htmlFor={id}>
+      {label + " "}
+      <textarea
+        id={id}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        disabled={isDisabled}
+      ></textarea>
+    </label>
+  );
+}
+
+export default App;
