f52e4c5bc527c55f60467354b06e04e8edc6e467
Belmouk <el.mehdi.boukhiam@gmail.com>
2026-01-06 14:53:54 +0100

    feature: add CV preview section for live update

--- src/App.jsx
+++ src/App.jsx
@@ -3,190 +3,215 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [content, setContent] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    schoolName: "",
+    schoolMajor: "",
+    graduationYear: "",
+    companyName: "",
+    role: "",
+    responsibilities: "",
+    jobStartDate: "",
+    jobEndDate: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setContent({ ...content, [name]: value });
+  }
+
   return (
-    <div>
-      <Section
-        title="General information"
-        inputs={[
-          {
-            tag: "input",
-            label: "Name",
-            type: "text",
-            name: "name",
-            id: "name",
-          },
-          {
-            tag: "input",
-            label: "Email",
-            type: "email",
-            name: "email",
-            id: "email",
-          },
-          {
-            tag: "input",
-            label: "Phone",
-            type: "tel",
-            name: "phone",
-            id: "phone",
-          },
-        ]}
-      ></Section>
-      <Section
-        title="Education"
-        inputs={[
-          {
-            tag: "input",
-            label: "School name",
-            type: "text",
-            name: "school",
-            id: "school",
-          },
-          {
-            tag: "input",
-            label: "Major",
-            type: "text",
-            name: "major",
-            id: "major",
-          },
-          {
-            tag: "input",
-            label: "Start date",
-            type: "date",
-            name: "school-start",
-            id: "school-start",
-          },
-          {
-            tag: "input",
-            label: "End date",
-            type: "date",
-            name: "school-end",
-            id: "school-end",
-          },
-        ]}
-      ></Section>
-      <Section
-        title="Professional Experience"
-        inputs={[
-          {
-            tag: "input",
-            label: "Company name",
-            type: "text",
-            name: "company-name",
-            id: "company-name",
-          },
-          {
-            tag: "input",
-            label: "Role",
-            type: "text",
-            name: "role",
-            id: "role",
-          },
-          {
-            tag: "textarea",
-            label: "Responsibilities",
-            type: undefined,
-            name: "responsibilities",
-            id: "responsibilities",
-          },
-          {
-            tag: "input",
-            label: "Start date",
-            type: "date",
-            name: "job-start-date",
-            id: "job-start-date",
-          },
-          {
-            tag: "input",
-            label: "End date",
-            type: "date",
-            name: "job-end-date",
-            id: "job-end-date",
-          },
-        ]}
-      ></Section>
-    </div>
+    <>
+      <header>
+        <h1>CV Generator</h1>
+      </header>
+      <main>
+        <div>
+          <Form
+            title="General information"
+            handleChange={handleChange}
+            inputs={[
+              {
+                tag: "input",
+                label: "Name",
+                type: "text",
+                name: "name",
+                id: "name",
+              },
+              {
+                tag: "input",
+                label: "Email",
+                type: "email",
+                name: "email",
+                id: "email",
+              },
+              {
+                tag: "input",
+                label: "Phone",
+                type: "tel",
+                name: "phone",
+                id: "phone",
+              },
+            ]}
+          ></Form>
+          <Form
+            title="Education"
+            handleChange={handleChange}
+            inputs={[
+              {
+                tag: "input",
+                label: "School name",
+                type: "text",
+                name: "schoolName",
+                id: "school",
+              },
+              {
+                tag: "input",
+                label: "Major",
+                type: "text",
+                name: "schoolMajor",
+                id: "major",
+              },
+              {
+                tag: "input",
+                label: "Graduation Year",
+                type: "number",
+                name: "graduationYear",
+                id: "graduation-year",
+              },
+            ]}
+          ></Form>
+          <Form
+            title="Professional Experience"
+            handleChange={handleChange}
+            inputs={[
+              {
+                tag: "input",
+                label: "Company name",
+                type: "text",
+                name: "companyName",
+                id: "company-name",
+              },
+              {
+                tag: "input",
+                label: "Role",
+                type: "text",
+                name: "role",
+                id: "role",
+              },
+              {
+                tag: "textarea",
+                label: "Responsibilities",
+                type: undefined,
+                name: "responsibilities",
+                id: "responsibilities",
+              },
+              {
+                tag: "input",
+                label: "Start date",
+                type: "date",
+                name: "jobStartDate",
+                id: "job-start-date",
+              },
+              {
+                tag: "input",
+                label: "End date",
+                type: "date",
+                name: "jobEndDate",
+                id: "job-end-date",
+              },
+            ]}
+          ></Form>
+        </div>
+        <div>
+          <section>
+            <h2>{content.name}</h2>
+            <ul>
+              <li>{content.email}</li>
+              <li>{content.phone}</li>
+            </ul>
+          </section>
+          <section>
+            <h2>Professional Experience</h2>
+            <h3>{content.companyName}</h3>
+            <div>
+              <p>{content.role}</p>
+              <p>
+                {content.jobStartDate} - {content.jobEndDate}
+              </p>
+            </div>
+            <p>{content.responsibilities}</p>
+          </section>
+          <section>
+            <h2>Education</h2>
+
+            <div>
+              {content.schoolName}, {content.schoolMajor} (
+              {content.graduationYear})
+            </div>
+          </section>
+        </div>
+      </main>
+      <footer>A well made CV is a job half-secured.</footer>
+    </>
   );
 }
 
-function Section({ title, inputs }) {
-  const [isDisabled, setIsDisabled] = useState(false);
-  function handleSubmit(e) {
-    e.preventDefault();
-    setIsDisabled(true);
-  }
-  function handleEdit(e) {
-    setIsDisabled(false);
-  }
-  
-  
-    return (
-      <form action="" autoComplete="on" onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>{title}</legend>
-          {inputs.map((item, index) => {
-            if (item.tag === "textarea") {
-              return (
-                <TextArea
-                  {...item}
-                  key={index}
-                  isDisabled={isDisabled}
-                ></TextArea>
-              );
-            }
+function Form({ title, inputs, handleChange }) {
+  return (
+    <form action="" autoComplete="on">
+      <fieldset>
+        <legend>{title}</legend>
+        {inputs.map((item, index) => {
+          if (item.tag === "textarea") {
             return (
-              <Input {...item} key={index} isDisabled={isDisabled}></Input>
+              <TextArea
+                {...item}
+                key={index}
+                handleChange={handleChange}
+              ></TextArea>
             );
-          })}
-          <div>
-            <button type="submit">Submit</button>
-            <button type="button" onClick={handleEdit}>
-              Edit
-            </button>
-          </div>
-        </fieldset>
-      </form>)
- 
-  
+          }
+          return (
+            <Input {...item} key={index} handleChange={handleChange}></Input>
+          );
+        })}
+      </fieldset>
+    </form>
+  );
 }
 
-function Input({ label, type, name, id, isDisabled }) {
-  const [value, setValue] = useState("");
-  function handleChange(e) {
-    setValue(e.target.value);
-  }
-
+function Input({ label, type, name, id, handleChange }) {
   return (
     <label htmlFor={id}>
       {label + " "}
-      <input
-        type={type}
-        name={name}
-        id={id}
-        value={value}
-        onChange={handleChange}
-        disabled={isDisabled}
-      />
+      <input type={type} name={name} id={id} onChange={handleChange} />
     </label>
   );
 }
 
-function TextArea({ id, name, label, isDisabled }) {
-  const [value, setValue] = useState("");
-
-  function handleChange(e) {
-    setValue(e.target.value);
-  }
+function TextArea({ id, name, label, handleChange }) {
   return (
     <label htmlFor={id}>
       {label + " "}
-      <textarea
-        id={id}
-        name={name}
-        value={value}
-        onChange={handleChange}
-        disabled={isDisabled}
-      ></textarea>
+      <textarea id={id} name={name} onChange={handleChange}></textarea>
     </label>
+  );
+}
+
+function Section({ title, points }) {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <h3>{heading}</h3>
+      <ul>
+        {points.map((point, index) => {
+          return <li key={index}>{point}</li>;
+        })}
+      </ul>
+    </section>
   );
 }
 
